f3b5ca507762f3fc7463fd532c7786d4306d7a9e
946818508@qq.com <scl199303>
2019-08-13 19:46:52 +0800

    *

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,46 @@
-import  React from 'react';
-import { LoginWrapper } from './style';
-import { Link } from "react-router-dom";
-class Home extends React.Component{
-    constructor(props){
+import React from 'react';
+import {
+    HomeWrapper,
+    TopFloor,
+    AnnouncementFloor,
+    Announcement,
+    NavigationFloor,
+    NavigationTab,
+    TabList,
+    RoomFloor,
+    BottomFloor} from './style';
+import {Link} from "react-router-dom";
+
+/*img resource*/
+import gonggao_bg from '../../resource/zhujiemian/gonggao.png';
+import lucky from '../../resource/zhujiemian/1.png';
+import signIn from '../../resource/zhujiemian/2.png';
+import bet from '../../resource/zhujiemian/3.png';
+import activity from '../../resource/zhujiemian/4.png';
+
+class Home extends React.Component {
+    constructor(props) {
         super(props);
     }
 
     render() {
         return (
-            <LoginWrapper>
-                <Link to="/auth">auth</Link>
-            </LoginWrapper>
+            <HomeWrapper>
+                <TopFloor/>
+                <AnnouncementFloor>
+                    <Announcement src={gonggao_bg}/>
+                </AnnouncementFloor>
+                <NavigationFloor>
+                    <TabList>
+                        <NavigationTab src={lucky}/>
+                        <NavigationTab src={signIn}/>
+                        <NavigationTab src={bet}/>
+                        <NavigationTab src={activity}/>
+                    </TabList>
+                </NavigationFloor>
+                <RoomFloor/>
+                <BottomFloor/>
+            </HomeWrapper>
         )
     }
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,7 +1,62 @@
 import styled from 'styled-components';
+import bg from '../../resource/zhujiemian/bg.jpg';
+import tou_bg from '../../resource/zhujiemian/tou bg.png';
 
-export const LoginWrapper = styled.div`
-    with:100%;
-    background:red;
+export const HomeWrapper = styled.div`
+    height: 100%;  
+    width: 100%;  
+    position: absolute;  
+    top: 0px;  
+    bottom: 0px;  
+    background: url('${bg}') no-repeat;  
+    background-size: 100%, 100%;
+    z-index: 0;  
+    overflow:hidden;
+    display: grid;
+    grid-template-rows: 8% 8% 11% 65% 8%;
 `;
 
+export const TopFloor = styled.div`
+    background: url('${tou_bg}') no-repeat;  
+    background-size: 100% 100%;
+`;
+
+export const AnnouncementFloor = styled.div`
+   
+`;
+
+export const Announcement = styled.img`
+    display:inline-block;
+    position:relative;
+    width:100%;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+
+export const NavigationFloor = styled.div`
+   
+`;
+
+export const TabList = styled.div`
+    display: grid;
+    width：100%;
+    height:100%;
+    grid-template-columns:25% 25% 25% 25%;
+`;
+
+export const NavigationTab = styled.img`
+    width:70%;
+    position:relative;
+    margin:0 auto;
+    top: 50%;
+    transform: translateY(-50%);
+`;
+
+
+export const RoomFloor = styled.div`
+   
+`;
+
+export const BottomFloor = styled.div`
+    
+`;
