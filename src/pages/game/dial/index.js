130676ad311002623ce3b1fe8802a4030599c9bc
946818508@qq.com <scl199303>
2019-08-14 19:17:21 +0800

    *

--- src/pages/game/dial/index.js
+++ src/pages/game/dial/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import {
     DialWrapper,
+    Back,
+    Announcement,
+    Tools,
+    Record,
+    Tab
 } from './style';
+import {Link} from "react-router-dom";
+import back from '../../../resource/youxijiemian/fanhui.png';
+import record from '../../../resource/youxijiemian/kaijiang.png';
+import servant from '../../../resource/youxijiemian/kefu.png';
+import rule from '../../../resource/youxijiemian/wanfa.png';
+import crowd from '../../../resource/youxijiemian/zaixian.png';
 
 class DialComponent extends React.Component{
     constructor(props) {
@@ -11,7 +22,18 @@ class DialComponent extends React.Component{
     render() {
         return (
             <DialWrapper>
-
+                <Link to='/'><Back src={back}/></Link>
+                <Announcement></Announcement>
+                <Tools>
+                    <div>
+                        <Record src={record}/>
+                    </div>
+                    <div>
+                        <Tab src={servant}/>
+                        <Tab src={rule}/>
+                        <Tab src={crowd}/>
+                    </div>
+                </Tools>
             </DialWrapper>
         )
     }
--- src/pages/game/dial/style.js
+++ src/pages/game/dial/style.js
@@ -2,6 +2,39 @@ import styled from 'styled-components';
 import bg from '../../../resource/youxijiemian/section1.png';
 
 export const DialWrapper = styled.div`
-      background: url('${bg}') center no-repeat 
+    background: url('${bg}') center no-repeat 
     background-size:100% 101%;
+`;
+
+export const Back = styled.img`
+    position:absolute;
+    top: 0px;
+    left:0;
+    width:80px;
+`;
+
+export const Announcement = styled.div`
+    background:rgba(0, 0, 0, 0.42);
+    width:100%;
+    height:25px;
+    line-height:25px;
+    padding-left:100px;
+`;
+
+export const Tools = styled.div`
+    width:100%;
+    height: 90%; 
+    display: grid;
+    grid-template-columns: auto 45px;
+`;
+
+export const Record = styled.img`
+    margin-top:40px;
+    width:30px;
+`;
+
+export const Tab = styled.img`
+    width:40px;
+    margin-top:10px;
+    display:block;
 `;
