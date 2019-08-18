b3f669314ab562f34b9f356360652ebe1ebb2f02
946818508@qq.com <scl199303>
2019-08-18 11:50:07 +0800

    *

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -2,15 +2,32 @@ import React from 'react';
 import {
     GameWrapper,
 } from './style';
+import {connect} from 'react-redux';
+import * as actions from './store/actions';
 /*component*/
 import DialComponent from './dial';
 import TableComponent from './table';
 import MoneyComponent from './money';
 import BetComponent from './bet';
+import axios from "axios";
+import {USER_INFO} from "./store/actions";
 
 class Game extends React.Component{
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount(){
+        axios.get('/api/userinfo.json').then((res) => {
+            const action = {
+                type: USER_INFO,
+                userInfo: res.data
+            };
+            this.props.getUserInfo(action);
+            console.log(this.props.userInfo)
+        }).catch(() => {
+
+        });
     }
 
     render() {
@@ -25,4 +42,18 @@ class Game extends React.Component{
     }
 }
 
-export default Game;
+const mapStateToProps = (state) => {
+    return {
+        userInfo:state.game.get('userInfo')
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getUserInfo(action){
+            dispatch(action);
+        }
+    }
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(Game);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import AuthReducer from '../pages/auth/store/reducer';
+import GameReducer from '../pages/game/store/reducer';
+
 
 export default combineReducers({
-    auth:AuthReducer
+    auth:AuthReducer,
+    game:GameReducer
 })
