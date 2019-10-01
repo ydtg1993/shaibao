05f68fc31a63ed1ee90d75a6cdcbb172fea8a62d
946818508@qq.com <scl199303>
2019-10-01 11:59:55 +0800

    *

--- src/pages/home/dialog/style.js
+++ src/pages/home/dialog/style.js
@@ -39,32 +39,6 @@ export const DialogContent = styled.div`
     background-image: radial-gradient(ellipse 100% 50% at bottom center,#1b4084,#1f263a);
 `;
 
-export const MongolianWrapper = styled.div`
-    height: 100%;  
-    width: 100%;  
-    position: absolute;  
-    top: 0px;  
-    bottom: 0px; 
-    z-index:999;
-    opacity: 0;
-    visibility: hidden;
-    background-color:rgba(0, 0, 0, 0.6); 
-    -webkit-transition: opacity .1s ease-in-out;
-    -moz-transition: opacity .1s ease-in-out;
-    -ms-transition: opacity .1s ease-in-out;
-    -o-transition: opacity .1s ease-in-out;
-    transition: opacity .1s ease-in-out;
-        
-    &.show {
-        opacity: 1;
-        visibility: visible;
-    }
-    
-    &.hidden {
-         opacity: 0;
-    }
-`;
-
 export const Close = styled.img.attrs({
     src:`${close}`
 })`
